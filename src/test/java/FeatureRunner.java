e697a03faadacd1162357faa956832e8b12cc816
Yavuz Kaya <yavuzkaya3866@gmail.com>
2020-12-07 15:52:40 +0100

    first 1

--- src/test/java/FeatureRunner.java
+++ src/test/java/FeatureRunner.java
@@ -1,8 +1,13 @@
+import com.intuit.karate.KarateOptions;
 import com.intuit.karate.junit4.Karate;
 import org.junit.runner.RunWith;
 
 @RunWith(Karate.class)
+@KarateOptions(
+
+        tags = "@wip"
+)
 public class FeatureRunner {
 
-    dsjdhk
+
 }
